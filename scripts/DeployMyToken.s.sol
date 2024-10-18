4d8eabdff7557ceafefe00a889a3538ecd14880a
check520 <sxpp316@gmail.com>
2024-10-18 13:38:13 +0800

    完成部署合约

--- scripts/DeployMyToken.s.sol
+++ scripts/DeployMyToken.s.sol
@@ -3,25 +3,14 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {MyFirstToken} from "../src/MyFirstToken.sol";
-//使用你在 Decert.met 登录的钱包来部署合约
-//0x377D734D0DAB9ee92f2649D31C3c3f48AdCa6c37
-/*forge script DeployMyToken --rpc-url  https://rpc.sepolia.org \
---private-key AA \
---broadcast */
+//使用你在 Decert.met 登录的钱包来部署合约 
 
 contract DeployMyToken is Script {
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-        MyFirstToken token = new MyFirstToken("MyToken", "MTK");
-        console.log("token address:", address(token));
-        console.log("token name:", token.name());
-        console.log("token symbol:", token.symbol());
-        console.log("token decimals:", token.decimals());
-        console.log("token totalSupply:", token.totalSupply());
-        address admin = address(0x377D734D0DAB9ee92f2649D31C3c3f48AdCa6c37);
-        token.transfer(admin, 100000000 * 10 ** 18);
+        MyFirstToken token = new MyFirstToken("MyToken", "MTK");  
         vm.stopBroadcast();
     }
 }
